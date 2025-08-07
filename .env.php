439686603e68838f190a377f7f0b8a2dc0ab524d
Laurent van der Straten <jumpingaccess@users.noreply.github.com>
2025-08-07 12:52:13 +0200

    Update .env.php

--- .env.php
+++ .env.php
@@ -4,10 +4,10 @@
 
 return [
     // Clé secrète JWT fournie par Equipe pour décoder les tokens
-    'EQUIPE_SECRET' => 'vabGHxF2QdDQt9R1',
+    'EQUIPE_SECRET' => 'Your_Equipe_Extension_Secret_Key',
     
     // Token Bearer pour l'authentification avec l'API Hippodata
-    'HIPPODATA_BEARER' => '9511562afc502fc009d30611036a185f',
+    'HIPPODATA_BEARER' => 'Hippodata_Bearer_Key',
 
     // DEBUG Inormation On/Off
     'DEBUG' => 1,
